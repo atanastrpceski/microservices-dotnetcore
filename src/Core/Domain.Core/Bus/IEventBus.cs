72b71489bd7cf5819e180948b15ef9845c70daea
Atanas Trpceski <atanastrpceski@gmail.com>
2020-03-18 09:37:32 +0100

    Init Commit

---
+++ src/Core/Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Core.Commands;
+using System.Threading.Tasks;
+using Domain.Core.Events;
+
+namespace Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+             where T : Event
+             where TH : IEventHandler<T>;
+    }
+}
---
+++ src/Core/Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
